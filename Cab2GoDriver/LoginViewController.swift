dea7a685c959f406da8b1a774771371da1c3eb2d
Yevhenii <yevhenii@macbook-pro-yevhenii.local>
2018-04-30 18:15:46 +0300

    Driver registration completed

--- Cab2GoDriver/LoginViewController.swift
+++ Cab2GoDriver/LoginViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    private let mapSegueIdentifier = "ShowMapVC"
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
@@ -17,11 +20,59 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         
     }
+    
+    //MARK:- Other Methods
+    
+    private func alertUser(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alerAction_OK = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(alerAction_OK)
+        present(alert, animated: true, completion: nil)
+    }
 
     @IBAction func signInButtonPressed(_ sender: UIButton) {
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { user, error in
+                
+                //Handle the error of signing up
+                if error != nil {
+                    
+                    
+                    
+                }
+                
+                self.performSegue(withIdentifier: self.mapSegueIdentifier, sender: self)
+                
+            })
+            
+        }
+        
     }
     
-    @IBAction func sugnUpButtonPressed(_ sender: UIButton) {
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            
+            UsersAuthorizationManager.authManager.signUp(email: emailTextField.text!, password: passwordTextField.text!, loginHandler: {message in
+                
+                if message != nil {
+                    //Message is not nil, so there is an error. Unable to register.
+                    self.alertUser(title: "Problem With Creating A New User", message: message!)
+                    
+                } else {
+                    //Registration is successful
+                    
+                    
+                }
+                
+            })
+            
+        } else {
+            
+            alertUser(title: "Email And Password Are Required", message: "Please, enter your Email and Password in the text fields")
+            
+        }
     }
     
 }
---
+++ Cab2GoDriver/Supporting/UsersAuthenticationManager.swift
@@ -0,0 +1,101 @@
+//
+//  UsersAuthentication.swift
+//  Cab2GoDriver
+//
+//  Created by Yevhenii on 21.04.2018.
+//  Copyright © 2018 Yevhenii. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+typealias LoginHandler = (_ message: String?) -> Void
+
+struct LoginErrorCodeMessage {
+    static let invalidEmailMessage = "Invalid Email Address. Please, Provide an Valid Email Address"
+    static let wrongPasswordMessage = "Wrong Password. Please, Enter The Correct Password"
+    static let problemConnectingMessage = "Cannot Connect to Database. Please, Try Again Later"
+    static let userNotFoundMessage = "User Not Found. Please, Sign Up"
+    static let emailAlreadyInUseMessage = "Email Is Already In Use. Please, Use Another Email"
+    static let weakPasswordMessage = "Password Should Be At Least 6 Characters Long"
+}
+
+class UsersAuthorizationManager {
+    
+    //Creating a singleton for this class
+    private static let instance = UsersAuthorizationManager()
+    
+    static var authManager: UsersAuthorizationManager {
+        
+        return instance
+        
+    }
+    
+    func signUp(email: String, password: String, loginHandler: LoginHandler?) {
+        
+        Auth.auth().createUser(withEmail: email, password: password, completion: {(user, error) in
+            
+            if error != nil {
+                self.handleErrors(error: error! as NSError, loginHandler: loginHandler)
+            } else {
+                
+                if user?.uid != nil {
+                    
+                    self.login(email: email, password: password, loginHandler: loginHandler)
+                }
+                
+            }
+            
+            
+        })
+        
+    }
+    
+    func login(email: String, password: String, loginHandler: LoginHandler?) {
+        
+        Auth.auth().signIn(withEmail: email, password: password, completion: { user, error in
+            
+            if error != nil {
+                
+                self.handleErrors(error: error! as NSError, loginHandler: loginHandler)
+                
+            } else {
+                
+                loginHandler?(nil)
+                
+            }
+            
+            //Handle user authorization
+            
+        })
+        
+    }
+    
+    private func handleErrors(error: NSError, loginHandler: LoginHandler?) {
+        
+        if let errorCode = AuthErrorCode(rawValue: error.code) {
+            
+            switch errorCode {
+                
+            case .wrongPassword:
+                loginHandler?(LoginErrorCodeMessage.wrongPasswordMessage)
+            case .invalidEmail:
+                loginHandler?(LoginErrorCodeMessage.invalidEmailMessage)
+            case .userNotFound:
+                loginHandler?(LoginErrorCodeMessage.userNotFoundMessage)
+            case .emailAlreadyInUse:
+                loginHandler?(LoginErrorCodeMessage.emailAlreadyInUseMessage)
+            case .weakPassword:
+                loginHandler?(LoginErrorCodeMessage.weakPasswordMessage)
+            default:
+                loginHandler?(LoginErrorCodeMessage.problemConnectingMessage)
+                
+            }
+            
+        }
+        
+    }
+    
+    
+}
+
